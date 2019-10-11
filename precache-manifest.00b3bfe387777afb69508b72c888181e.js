da63500445ec8fe9252f9a9720d70090e0491bd8
Demarkus Matthews <dmanthony@Demarkuss-MacBook-Pro.local>
2019-10-11 11:15:04 -0700

    Updates

---
+++ precache-manifest.00b3bfe387777afb69508b72c888181e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b151b47b921411795847ed1e46422545",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "82994823e750ce2c0e62",
+    "url": "/home/static/js/2.39b909fe.chunk.js"
+  },
+  {
+    "revision": "d198f1638b022059a30c",
+    "url": "/home/static/js/main.dc69e8ce.chunk.js"
+  },
+  {
+    "revision": "54b14840dfdc606c639a",
+    "url": "/home/static/js/runtime-main.ef6b07be.js"
+  }
+]);
